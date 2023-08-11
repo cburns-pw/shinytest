1349d5b39cca1706e879fa73191fc807369f4edc
Christian Burns <cburns@parkwestinc.com>
2023-08-11 14:59:48 -0700

    Add project files.

---
+++ ShinyTest/MyPushDelegate.cs
@@ -0,0 +1,26 @@
+﻿using Shiny.Push;
+using System.Diagnostics;
+
+namespace ShinyTest
+{
+    public class MyPushDelegate : IPushDelegate
+    {
+        public Task OnEntry(PushNotification push)
+        {
+            Debug.WriteLine("OnEntry");
+            return Task.CompletedTask;
+        }
+
+        public Task OnReceived(PushNotification push)
+        {
+            Debug.WriteLine("OnReceived");
+            return Task.CompletedTask;
+        }
+
+        public Task OnTokenRefreshed(string token)
+        {
+            Debug.WriteLine($"OnTokenRefreshed: {token}");
+            return Task.CompletedTask;
+        }
+    }
+}
